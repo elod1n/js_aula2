8e1f59161cdf6f8f8cbad4dec556c67d639dd26f
Bruno Dias <bruno@freterapido.com>
2023-10-18 21:49:48 -0300

    orientação de preencher os valores separando com ponto

--- js/aula2.js
+++ js/aula2.js
@@ -1,9 +1,9 @@
 function runCalculator() {
     let nomeUsuario = prompt("Bem vindo a calculadora de imposto de importação. Digite seu primeiro nome para que você possa utilizar a calculadora.");
 
-    let dolarValor = parseFloat(prompt(`${nomeUsuario}, qual é a cotação do dólar US$ no dia de hoje (valor numérico)?`));
+    let dolarValor = parseFloat(prompt(`${nomeUsuario}, qual é a cotação do dólar US$ no dia de hoje (separe a casa decimal com ponto)?`));
 
-    let valorProduto = parseFloat(prompt(`${nomeUsuario}, qual é o valor do produto em reais (R$) que você deseja comprar?`));
+    let valorProduto = parseFloat(prompt(`${nomeUsuario}, qual é o valor do produto em reais (R$) que você deseja comprar (separe a casa decimal com ponto)?`));
 
     let valorConvertido = valorProduto / dolarValor;
 
